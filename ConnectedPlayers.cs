3689346f0c7130b191b3b8f77a7a673fc1754601
HGM <cod5cfg1@gmail.com>
2025-08-08 11:43:34 +0100

    Add files via upload

---
+++ ConnectedPlayers.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SharedLibraryCore;
+using SharedLibraryCore.Helpers;
+using SharedLibraryCore.Interfaces;
+using SharedLibraryCore.Interfaces.Events;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConnectedPlayers
+{
+    public class Plugin : IPluginV2
+    {
+        public const string PluginName = "ConnectedPlayers";
+        public string Name => PluginName;
+        public string Version => "2025-04-10";
+        public string Author => "HGM";
+
+        public static void RegisterDependencies(IServiceCollection services) { }
+
+        public Plugin()
+        {
+            IManagementEventSubscriptions.Load += OnLoad;
+        }
+
+        private Task OnLoad(IManager manager, CancellationToken token)
+        {
+            Console.WriteLine($"[{Name}] by {Author} loaded. Version: {Version}");
+
+            manager.GetMessageTokens().Add(
+                new MessageToken("CONNECTED", (s) =>
+                    Task.FromResult(manager.GetActiveClients().Count.ToString())
+                )
+            );
+
+            return Task.CompletedTask;
+        }
+    }
+}
